156e2fd83ea68f6cd1b9a8b32c62c00ec51f9b73
irisk <s1200936@student.windesheim.nl>
2024-11-21 16:17:45 +0100

    opdracht 1, 2 en 3 tekstfuncties

--- opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
+++ opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
@@ -1,9 +1,10 @@
 # Opdracht 1
-# Naam student:
+# Naam student: Elliot Kuster
 # Groep:
 
 # Hier komt je code...
+my_string = "Tinus gaat op zijn tandem naar de hottentottententoonstelling"
 
-my_list = ...
+my_list = my_string.count("t")
 
-print()
+print("De letter t komt",my_list,"x voor in my_string")
--- opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
@@ -1,5 +1,7 @@
 # Opdracht 3 tekstfuncties
-# Naam student:
+# Naam student: Elliot Kuster
 # Groep:
 
 # Hier komt je code...
+kerstboom= "    *\n   ***\n  ******\n ********\n***********\n    ***\n    ***\n    ***\n\n"
+print(kerstboom*5)
