9a6e5942f1fd0d3b43ed59bb26c01c098359ac6a
irisk <s1200936@student.windesheim.nl>
2024-11-28 10:23:36 +0100

    lists opdr 1

--- opdrachten/040_lists/opdr_1/opdr_1.py
+++ opdrachten/040_lists/opdr_1/opdr_1.py
@@ -1,9 +1,10 @@
 # Opdracht 1 lists
-# Naam student:
+# Naam student: Elliot Kuster
 # Groep:
 
-mylist = ...
-dict_1 = ...
-dict_2 = ...
-dict_3 = ...
-dict_4 = ...
+mylist = []
+dict_1 = mylist.append({"id":0,"voornaam":"joep","achternaam":"poep","huisdier":"kat"})
+dict_2 = mylist.append({"id":1,"voornaam":"tim","achternaam":"pim","huisdier":"slak"})
+dict_3 = mylist.append({"id":2,"voornaam":"henk","achternaam":"tenk","huisdier":"varken"})
+dict_4 = mylist.append({"id":3,"voornaam":"klaas","achternaam":"gaas","huisdier":"eend"})
+print(mylist[1]["voornaam"],mylist[1]["achternaam"])
