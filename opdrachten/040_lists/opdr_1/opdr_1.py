2f59c540892b41ee2acd339e1860dc646c4eee21
irisk <s1200936@student.windesheim.nl>
2024-11-28 12:27:42 +0100

    lists opdr 1 en 2

--- opdrachten/040_lists/opdr_1/opdr_1.py
+++ opdrachten/040_lists/opdr_1/opdr_1.py
@@ -3,8 +3,8 @@
 # Groep:
 
 mylist = []
-dict_1 = mylist.append({"id":0,"voornaam":"joep","achternaam":"poep","huisdier":"kat"})
-dict_2 = mylist.append({"id":1,"voornaam":"tim","achternaam":"pim","huisdier":"slak"})
-dict_3 = mylist.append({"id":2,"voornaam":"henk","achternaam":"tenk","huisdier":"varken"})
-dict_4 = mylist.append({"id":3,"voornaam":"klaas","achternaam":"gaas","huisdier":"eend"})
+dict_1 = {"id":0,"voornaam":"joep","achternaam":"poep","huisdier":"kat"}
+dict_2 = {"id":1,"voornaam":"tim","achternaam":"pim","huisdier":"slak"}
+dict_3 = {"id":2,"voornaam":"henk","achternaam":"tenk","huisdier":"varken"}
+dict_4 = {"id":3,"voornaam":"klaas","achternaam":"gaas","huisdier":"eend"}
 print(mylist[1]["voornaam"],mylist[1]["achternaam"])
--- opdrachten/040_lists/opdr_2/opdr_2.py
+++ opdrachten/040_lists/opdr_2/opdr_2.py
@@ -1,5 +1,5 @@
 # Opdracht 2 lists
-# Naam student:
+# Naam student: Elliot Kuster
 # Groep:
 
 
@@ -13,3 +13,11 @@ rivieren = list(rivier_info.keys())
 # rivieren is nu een list met alleen de riviernamen: ['rijn', 'maas', 'nijl']
 
 # Hier jouw code.....
+print(rivieren[0])
+print(f"De rivier {rivieren[0].title()} onder andere door {rivier_info['rijn'][1].title()}")
+
+print(rivieren[1])
+print(f"De rivier {rivieren[0].title()} onder andere door {rivier_info['rijn'][0].title()}")
+
+print(rivieren[2])
+print(f"De rivier {rivieren[2].title()} onder andere door {rivier_info['nijl'][2].title()}")
