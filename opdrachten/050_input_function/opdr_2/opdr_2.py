6d90388bb656e5cafe7879487dc8670067b6645d
irisk <s1200936@student.windesheim.nl>
2024-12-04 11:54:39 +0100

    input function opdr 2

--- opdrachten/050_input_function/opdr_2/opdr_2.py
+++ opdrachten/050_input_function/opdr_2/opdr_2.py
@@ -4,4 +4,11 @@
 
 # Hier komt je code...
 
-gasten = ["Jij", ... ]
+gasten = ["Jij", "Paul","Kees","Marie","Hilda" ]
+print(gasten)
+
+gasten.remove("Hilda")
+print(gasten)
+
+gasten.insert(3,"George")
+print(gasten)
