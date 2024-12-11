2b2ae5886710929e2619c73c28a6e43f4bd4e340
irisk <s1200936@student.windesheim.nl>
2024-12-11 20:08:30 +0100

    080 tekstbestanden opdr 1 en 2

--- opdrachten/080_tekstbestanden/opdr_1/opdr_1.py
+++ opdrachten/080_tekstbestanden/opdr_1/opdr_1.py
@@ -1,5 +1,25 @@
 # Opdracht 1 while-loops
-# Naam student:
+# Naam student: Elliot Kuster
 # Groep:
 
 # Jouw code komt hier
+vraag1 = 'Wat vind je van de huidige regering?'
+vraag2 = 'Wat vind je van de Python-lessen tot nu toe?'
+vraag3 = 'Wat vind jij de mooiste stad van Nederland?'
+
+active = True
+enquete = []
+while active:
+    message = input(f"{vraag1}\n")
+    message2 = input(f"{vraag2}\n")
+    message3 = input(f"{vraag3}\n")
+    if message == 'q':
+        active = False
+        fo = open('newtext.txt', 'wt')
+        for antwoord in enquete:
+            fo.write(antwoord + "\n")
+        fo.close()
+    else:
+        enquete.append(message)
+        enquete.append(message2)
+        enquete.append(message3)
--- opdrachten/080_tekstbestanden/opdr_2/opdr_2.py
+++ opdrachten/080_tekstbestanden/opdr_2/opdr_2.py
@@ -1,5 +1,5 @@
 # Opdracht 2 tekstbestanden
-# Naam student:
+# Naam student: Elliot Kuster
 # Groep:
 
 import random
@@ -7,4 +7,19 @@ prompt = "Raad mijn geheime getal \n"
 geheim_getal = random.randint(1, 100)
 
 # De rest moet jij doen.....
-
+active = True
+keren = 1
+while True:
+    gok = int(input(prompt))
+    if geheim_getal < gok:
+        keren +=1
+        print("lager")
+        continue
+    elif geheim_getal > gok:
+        keren+=1
+        print("hoger")
+        continue
+    else: active = False
+    print(f"Hoera! het geraden getal is {geheim_getal} \n")
+    print(f"je hebt het in {(keren)} gedaan")
+    break
