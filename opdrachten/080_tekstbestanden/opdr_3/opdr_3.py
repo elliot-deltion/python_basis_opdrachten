9b8c301d4e0353e5ba514c1217c0e17d57284229
irisk <s1200936@student.windesheim.nl>
2024-12-11 21:12:24 +0100

    080 tekstbestanden opdr 3

--- opdrachten/080_tekstbestanden/opdr_3/opdr_3.py
+++ opdrachten/080_tekstbestanden/opdr_3/opdr_3.py
@@ -1,6 +1,23 @@
 # Opdracht 3 Tekst opslaan
-# Naam student:
+# Naam student: Elliot Kuster
 # Groep:
 
+tekst = input("Geef de tekst die wilt encrypten: \n")
+tekst_encrypted = ""
 
+for teken in tekst:
+    if teken == " ":
+        tekst_encrypted += " "
+    elif teken == ",":
+        tekst_encrypted += ","
+    elif "a" <= teken <= "z":
+        tekst_code = ord(teken) - ord("a")
+        tekst_code_encrypted = (tekst_code+5) % 26
+        tekst_encrypted += chr(tekst_code_encrypted + ord("a"))
+    else: 
+        "A" <= teken <= "Z"
+        tekst_code = ord(teken) - ord("A")
+        tekst_code_encrypted = (tekst_code+5) % 26
+        tekst_encrypted += chr(tekst_code_encrypted + ord("A"))
 
+print(tekst_encrypted)
