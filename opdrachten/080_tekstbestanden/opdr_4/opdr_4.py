110703b2b5e7eb429add584cf334b00e06360df8
irisk <s1200936@student.windesheim.nl>
2024-12-16 22:02:16 +0100

    080 tekstbestanden opdr 4 bijna helemaal

--- opdrachten/080_tekstbestanden/opdr_4/opdr_4.py
+++ opdrachten/080_tekstbestanden/opdr_4/opdr_4.py
@@ -1,6 +1,28 @@
 # Opdracht 4 Tekst opslaan
-# Naam student:
+# Naam student: Elliot Kuster
 # Groep:
 
 
 # Party enquete
+vraag1 = "Wat is je voornaam?"
+vraag2 = "Wat is je achternaam?"
+vraag3 = "Wat neem je mee aan drank?"
+vraag4 = "Wat neem je mee om te eten?"
+
+active = True
+feestje = []
+nummer = 0
+while active:
+        antwoord1 = input(f"{nummer+1}. {vraag1}\n")
+        antwoord2 = input(f"{nummer+2}. {vraag2}\n")
+        antwoord3 = input(f"{nummer+3}. {vraag3}\n")
+        antwoord4 = input(f"{nummer+4}. {vraag4}\n")
+        if antwoord1 == 'q':
+                active = False
+                fo = open('enquete.txt', 'at')
+                for answer in feestje:
+                    fo.write(f"{answer}\n")
+                fo.close()
+        else: lijstje = {'naam':antwoord1,'achternaam':antwoord2,'drank':antwoord3,'eten':antwoord4}
+        feestje.append(lijstje)
+        print("Bedankt voor het invullen! \nSee you at the party!")
